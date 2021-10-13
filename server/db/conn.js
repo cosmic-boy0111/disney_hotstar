50165d47ae5f5225b28572580c2dbfb689f3a5ea
cosmic-boy0111 <87473128+cosmic-boy0111@users.noreply.github.com>
2021-10-13 15:57:25 +0530

    server changes

--- server/db/conn.js
+++ server/db/conn.js
@@ -5,9 +5,9 @@ const DB = process.env.DATABASE;
 
 mongoose.connect(DB, {
     useNewUrlParser: true,
-    useCreateIndex: true,
+    // useCreateIndex: true,
     useUnifiedTopology: true,
-    useFindAndModify: false
+    // useFindAndModify: false
 }).then(()=>{
     console.log('connection successfull');
 }).catch((err)=> console.log('no connection'))
